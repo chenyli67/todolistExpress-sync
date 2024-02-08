9bd63f719c302b601193184cf70e2713ee24f494
chenyli <chenyuan.li@capgemini.com>
2024-02-09 00:45:46 +0800

    fix the port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const app = express()
-const port = 3002
+const port = 3000
 import{router} from  './route.js'
 
 app.use(express.json())
-app.use('/api/v2',router)
+app.use('/api/v3',router)
 
 app.listen(port, () => {
   console.log(`Server is running  on port ${port}`)
